967763a7a710d2e3fbec81d1a1ff97b3aeb239c3
T Q D <le7237439@gmail.com>
2026-04-06 12:17:27 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "dương dành tặng người mình thương",
+    introTitle: 'ai đó à',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '💗 Click 💗',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Yêu lắm 💝',
+    btnNo: 'Không nha 😂',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao ai đó lại yêu mình 🤣',
+    btnReply: '💖 Gửi cho Dương 💖',
+    reply: 'Yêu thì yêu mà không yêu thì yêu 😋',
+    mess: '    Dương biết mà 🥰. Yêu ai đó nhiều nhiều 😘😘',
+    messDesc: 'Anh hứa , sau này có tiền anh sẽ nuôi em thành HEO.',
+    btnAccept: 'Okiiiii lun 🥰',
+    messLink: 'https://github.com/duongbui23407/to-tinh-crush.git' //link mess của các bạn.
+}
